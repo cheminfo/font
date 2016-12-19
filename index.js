366641dd3d80a11e13b994a1ecd6e2a460e833c6
Luc Patiny <luc@patiny.com>
2016-12-19 17:53:49 +0100

    New approache with only one css file generated

--- index.js
+++ index.js
@@ -1,21 +1,96 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
-const webfontsGenerator = require('webfonts-generator');
-var files = fs.readdirSync(__dirname + '/svg');
+const prefix='ci-icon';
+const source=__dirname + '/svg';
+const destination=__dirname + '/dist/';
 
-files=files.filter(function(file) {
-    return file.match(/svg$/);
-})
+var resultsCss=[];
+var resultsHtml=[];
+appendHeaderCss();
+appendHeaderHtml();
 
-webfontsGenerator({
-    files: files.map(file => path.join('svg', file)),
-    dest: 'dist/',
-    html: true,
-    fontName: 'cheminfo',
-    templateOptions: {
-        classPrefix: 'ci-icon-',
-        baseClass: 'ci-icon'
-    }
-});
+appendFolder(source);
+
+appendFooterHtml();
+
+fs.writeFileSync(destination+'style.css', resultsCss.join('\r\n'));
+fs.writeFileSync(destination+'test.html', resultsHtml.join('\r\n'));
+
+
+function appendFolder(folder) {
+    var files = fs.readdirSync(folder);
+    files.forEach(function(file) {
+        var fullname=folder+'/'+file;
+        if (fs.lstatSync(fullname).isDirectory()) {
+            appendFolder(fullname);
+        } else if (file.match(/svg$/)) {
+            var icon=fullname.replace(source+'/','').replace(/\//g,'-').replace(/\..[^\.]*$/g,'');
+            console.log(icon);
+            var base64 = fs.readFileSync(fullname, 'base64');
+            var dataUrl = 'data:image/svg+xml;base64,' + base64;
+            resultsCss.push(`
+.${prefix}-${icon} {
+        content: url('${dataUrl}');
+}
+     `)
+
+            resultsHtml.push(`
+<div class="preview" style="font-size: 12px">
+	<span class="preview_icon">
+		<span class="${prefix} ${prefix}-${icon}"></span>
+	</span>
+	<span>${icon}</span>
+</div>            
+            `)
+        }
+    })
+}
+
+
+
+function appendHeaderCss() {
+    resultsCss.push(`
+.${prefix} {
+        width: 1em;
+        height: 1em;
+}
+    `);
+}
+
+
+function appendHeaderHtml() {
+    resultsHtml.push(`
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<style>
+	body {
+			font-family: sans-serif;
+			margin: 0;
+			padding: 10px 20px;
+		}
+
+		.preview {
+			line-height: 1.5em;
+		}
+		.preview_icon {
+		    font-size: 2em
+		}
+
+</style>
+	<link rel="stylesheet" type="text/css" href="./style.css">
+</head>
+<body>
+	
+    `);
+}
+
+function appendFooterHtml() {
+    resultsHtml.push(`
+</body>
+</html>	
+    `);
+}
+
+
