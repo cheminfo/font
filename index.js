dd9081b7733425f9fe9ae515ec07958cf5f1f92b
Luc Patiny <luc@patiny.com>
2016-12-20 13:01:12 +0100

    Use directly the svg in the css

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ fs.writeFileSync(destination+'index.html', resultsHtml.join('\r\n'));
 
 function appendFolder(folder) {
     var files = fs.readdirSync(folder);
-    
+
     files.forEach(function(file) {
         var fullname=folder+'/'+file;
         if (fs.lstatSync(fullname).isDirectory()) {
@@ -45,7 +45,8 @@ function appendFolder(folder) {
             var svg = fs.readFileSync(fullname, 'utf-8');
             
             svgo.optimize(svg, function(svg) {
-                var dataUrl = 'data:image/svg+xml;base64,' + btoa(svg.data);
+                //var dataUrl = 'data:image/svg+xml;base64,' + btoa(svg.data);
+                var dataUrl = 'data:image/svg+xml;utf-8,' + svg.data.replace(/'/g,'\\');
                 appendEntryCss(resultsCss,dataUrl, prefix, icon);
                 appendEntryHtml(resultsHtml,prefix, icon);
             })
